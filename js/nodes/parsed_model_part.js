af7371eec0c60b1c8f39b0c2d94a3e49d2499d16
Carlos Roig <roigcarlo@gmail.com>
2020-09-17 16:59:42 +0200

    Refactored to modern js. Thx @adrigzr

--- js/nodes/parsed_model_part.js
+++ js/nodes/parsed_model_part.js
@@ -1,70 +1,66 @@
-function ParsedModelPart() {
-    var self = this; // Not sure if this is clean :S
+class ParsedModelPart {
+    constructor() {
+        this.input_manager = document.createElement('input');
+        this.input_manager.type = 'file';
+        this.input_manager.addEventListener('change', this.onSelection.bind(this));
 
-    this.input_manager = document.createElement('input');
-    this.input_manager.type = 'file';
+        this.mp_select = this.addWidget("button", "Load Mdpa", "", function (value, widget, node) {
+            console.log(node.input_manager.value);
+            node.input_manager.click();
+        });
 
-    this.input_manager.addEventListener('change', (event) => {
+        this.size = this.computeSize();
+        this.serialize_widgets = true;
+    }
+
+    onExecute() {
+        // Currently does nothing
+    }
+
+    onSelection(e) {
         const fileList = event.target.files;
+        this.readSingleFile(fileList[0]);
+    }
 
-        function readSingleFile(file) {                
-            if (!file) {
-                console.log("Not a file?");
-                return;
+    readSingleFile(file) {
+        if (!file) {
+            return;
+        }
+
+        var reader = new FileReader();
+
+        reader.onload = this.onReaderLoad(file);
+        reader.readAsText(file);
+    };
+
+    onReaderLoad(file) {
+        return (e) => {
+            let contents = e.target.result;
+            let mdpa_subs_re = /.*((Begin SubModelPart) ([a-zA-Z0-9_]+))|(End SubModelPart$)/gm;
+            let sub_mdpa = contents.matchAll(mdpa_subs_re);
+            console.log(sub_mdpa);
+            // Remove existing outputs
+            while (this.outputs != undefined && this.outputs.length != 0) {
+                this.removeOutput(0);
             }
-
-            var reader = new FileReader();
-
-            reader.onload = function(e) {
-                var contents = e.target.result;
-
-                let mdpa_subs_re = /.*((Begin SubModelPart) ([a-zA-Z0-9_]+))|(End SubModelPart$)/gm
-
-                let sub_mdpa = contents.matchAll(mdpa_subs_re);
-
-                console.log(sub_mdpa);
-
-                // Remove existing outputs
-                while (self.outputs != undefined && self.outputs.length != 0) {
-                    self.removeOutput(0);
+            // Obtain the name of the ModelPart to get complete routes
+            var sub_mdpa_namepath = file.name.slice(0, -5);
+            for (var match of sub_mdpa) {
+                console.log(match);
+                if (match[0].includes("Begin")) {
+                    sub_mdpa_namepath = sub_mdpa_namepath + "." + match[3];
+                    this.addOutput(sub_mdpa_namepath, "string");
                 }
-                
-                // Obtain the name of the ModelPart to get complete routes
-                var sub_mdpa_namepath = file.name.slice(0,-5);
-
-                for(var match of sub_mdpa) {
-                    console.log(match);
-                    if(match[0].includes("Begin")) {
-                        sub_mdpa_namepath = sub_mdpa_namepath + "." + match[3]
-                        self.addOutput(sub_mdpa_namepath,"string");
-                    } else {
-                        sub_mdpa_namepath = sub_mdpa_namepath.split(".").slice(0,-1).join(".");
-                    }
+                else {
+                    sub_mdpa_namepath = sub_mdpa_namepath.split(".").slice(0, -1).join(".");
                 }
-            };
-
-            reader.readAsText(file);
-        };
-
-        readSingleFile(fileList[0]);
-    });
-
-    this.mp_select = this.addWidget("button", "Load Mdpa", "", function(value, widget, node) {
-        console.log(node.input_manager.value)
-        node.input_manager.click();
-    });
-
-    // this.addOutput("Name","string");
-    this.size = this.computeSize();
-    this.serialize_widgets = true;
+            }
+        }
+    }
 }
 
 ParsedModelPart.title = "ParsedModelPart";
 ParsedModelPart.desc = "view of an Output object";
-
-ParsedModelPart.prototype.onExecute = function() {
-    // this.setOutputData(0,  this.mp_name.value);
-};
 
 LiteGraph.registerNodeType("model_part/ParsedModelPart", ParsedModelPart);
 
