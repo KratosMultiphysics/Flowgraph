c3c91f0c1ef26b0aa266875845599c222e9a5719
Carlos Roig <roigcarlo@gmail.com>
2020-09-17 17:32:43 +0200

    Using correct variables

--- js/nodes/parsed_model_part.js
+++ js/nodes/parsed_model_part.js
@@ -27,7 +27,7 @@ class ParsedModelPart {
             return;
         }
 
-        var reader = new FileReader();
+        const reader = new FileReader();
 
         reader.onload = this.onReaderLoad(file);
         reader.readAsText(file);
@@ -35,16 +35,19 @@ class ParsedModelPart {
 
     onReaderLoad(file) {
         return ({ target: { result } }) => {
-            let mdpa_subs_re = /.*((Begin SubModelPart) ([a-zA-Z0-9_]+))|(End SubModelPart$)/gm;
-            let sub_mdpa = result.matchAll(mdpa_subs_re);
+            const mdpa_subs_re = /.*((Begin SubModelPart) ([a-zA-Z0-9_]+))|(End SubModelPart$)/gm;
+            const sub_mdpa = result.matchAll(mdpa_subs_re);
 
             // Remove existing outputs
             while (this.outputs != undefined && this.outputs.length != 0) {
                 this.removeOutput(0);
             }
+
             // Obtain the name of the ModelPart to get complete routes
-            var sub_mdpa_namepath = file.name.slice(0, -5);
-            for (var match of sub_mdpa) {
+            let sub_mdpa_namepath = file.name.slice(0, -5);
+
+            // Obtain the Submodelparts
+            for (const match of sub_mdpa) {
                 if (match[0].includes("Begin")) {
                     sub_mdpa_namepath = `${sub_mdpa_namepath}.${match[3]}`;
                     this.addOutput(sub_mdpa_namepath, "string");
@@ -52,6 +55,8 @@ class ParsedModelPart {
                     sub_mdpa_namepath = sub_mdpa_namepath.split(".").slice(0, -1).join(".");
                 }
             }
+
+            this.size = this.computeSize();
         }
     }
 }
