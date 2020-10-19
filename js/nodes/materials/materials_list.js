6012ead687a9cdea8d3708957421c01da9eb7c27
rubenzorrilla <rubenzorrillamartinez@hotmail.com>
2020-10-19 16:54:03 +0200

    1st material support

---
+++ js/nodes/materials/materials_list.js
@@ -0,0 +1,17 @@
+
+class MaterialsList extends InputList{
+    constructor() {
+        super();
+        this.input_type = "material";
+        this.output_type = "json";
+    }
+};
+
+MaterialsList.title = "Materials list";
+MaterialsList.desc = "Merges several materials into an array";
+
+LiteGraph.registerNodeType("materials/materials_list", MaterialsList);
+
+console.log("Materials list node created"); //helps to debug
+
+    
