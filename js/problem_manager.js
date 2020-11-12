e225e10513a064afb6b033e93f3d5749960d5628
roigcarlo <roigcarlo@gmail.com>
2020-11-12 11:21:39 +0000

    Implemented modelpart selector proxy

--- js/problem_manager.js
+++ js/problem_manager.js
@@ -1,3 +1,5 @@
-let problem_files = {}
+let problem_files = {}      // List of files loaded in the workspace
+let problem_modelparts = {} // List of modelparts loaded in the workspace
+let model_nodes = {}        // List of 'Models' nodes in the workspace
 
 problem_files["materials"] = {}
