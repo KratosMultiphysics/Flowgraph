a046037bd557deeb93c2717eb3d829563e1b319a
roigcarlo <roigcarlo@gmail.com>
2021-02-03 11:27:11 +0000

    Adding support for some cosmetics

--- js/problem_manager.js
+++ js/problem_manager.js
@@ -3,3 +3,5 @@ let problem_modelparts = {} // List of modelparts loaded in the workspace
 let model_nodes = {}        // List of 'Models' nodes in the workspace
 
 problem_files["materials"] = {}
+
+LGraphCanvas.link_type_colors["string"] = "#F0F"
