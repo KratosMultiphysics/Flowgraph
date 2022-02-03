6d9cadcf4feb4bbae6ef2ae8994b29b8066dff74
roigcarlo <roigcarlo@gmail.com>
2022-02-03 16:26:03 +0000

    Adding custom configure function into LGraph
    Default function has some behaviour I don't understand:
    - this._nodes gets deleted regardless of the 'keep' variable
    - links are not added, but replaced directly into the class
    
    Until I figure out why is this done this way import node selection
    need to be done with my function

---
+++ js/injections/selection_import.js
@@ -0,0 +1,99 @@
+    /**
+     * Configure a graph from a JSON string
+     * @method configure
+     * @param {String} str configure a graph from a JSON string
+     * @param {Boolean} returns if there was any error parsing
+     */
+     LGraph.prototype.configure_selection = function(data, keep_old) {
+        if (!data) {
+            return;
+        }
+
+        if (!keep_old) {
+            this.clear();
+        }
+
+        var nodes = data.nodes;
+
+        //decode links info (they are very verbose)
+        if (data.links && data.links.constructor === Array) {
+            var links = [];
+            for (var i = 0; i < data.links.length; ++i) {
+                var link_data = data.links[i];
+				if(!link_data) //weird bug
+				{
+					console.warn("serialized graph link data contains errors, skipping.");
+					continue;
+				}
+                var link = new LiteGraph.LLink();
+                link.configure(link_data);
+                this.links[link.id] = link;
+            }
+            // data.links = links;
+        }
+
+        //copy all stored fields
+        for (var i in data) {
+			if(i == "nodes" || i == "groups" || i == "links") //links must be accepted
+				continue;
+            this[i] = data[i];
+        }
+
+        var error = false;
+
+        //create nodes
+        // this._nodes = [];
+        if (nodes) {
+            for (var i = 0, l = nodes.length; i < l; ++i) {
+                var n_info = nodes[i]; //stored info
+                var node = LiteGraph.createNode(n_info.type, n_info.title);
+                if (!node) {
+                    if (LiteGraph.debug) {
+                        console.log(
+                            "Node not found or has errors: " + n_info.type
+                        );
+                    }
+
+                    //in case of error we create a replacement node to avoid losing info
+                    node = new LGraphNode();
+                    node.last_serialization = n_info;
+                    node.has_errors = true;
+                    error = true;
+                    //continue;
+                }
+
+                node.id = n_info.id; //id it or it will create a new id
+                this.add(node, true); //add before configure, otherwise configure cannot create links
+            }
+
+            //configure nodes afterwards so they can reach each other
+            for (var i = 0, l = nodes.length; i < l; ++i) {
+                var n_info = nodes[i];
+                var node = this.getNodeById(n_info.id);
+                if (node) {
+                    node.configure(n_info);
+                }
+            }
+        }
+
+        //groups
+        this._groups.length = 0;
+        if (data.groups) {
+            for (var i = 0; i < data.groups.length; ++i) {
+                var group = new LiteGraph.LGraphGroup();
+                group.configure(data.groups[i]);
+                this.add(group);
+            }
+        }
+
+        this.updateExecutionOrder();
+
+		this.extra = data.extra || {};
+
+		if(this.onConfigure)
+			this.onConfigure(data);
+
+        this._version++;
+        this.setDirtyCanvas(true, true);
+        return error;
+    };
