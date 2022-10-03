9c3a20c03ea73e121bca754df5885cc198efce64
Marcelo Raschi <marcelo.raschi@gmail.com>
2022-10-03 13:33:35 +0200

    fix solver setting input type. node category.

--- js/nodes/project_parameters.js
+++ js/nodes/project_parameters.js
@@ -1,11 +1,12 @@
 function ProjectParameters() {
     this.addInput("problem_data", "map");
-    this.addInput("output_processes", "process_array");
-    this.addInput("solver_settings", "map");
+//
+    this.addInput("solver_settings", "solver_settings");
     this.addInput("initial_conditions_process_list", "process_array");
     this.addInput("boundary_conditions_process_list", "process_array");
     this.addInput("gravity", "map");
     this.addInput("auxiliar_process_list", "process_array");
+    this.addInput("output_processes", "process_array");
 
     this.addOutput("project_parameters","map");
     this.size = this.computeSize();
@@ -16,19 +17,20 @@ ProjectParameters.desc = "create a Project Parmaters";
 
 ProjectParameters.prototype.onExecute = function() {
     tmp = {};
-    tmp["processes"] = {};
 
-    tmp["problem_data"] = this.getInputData(0); //0
-    tmp["output_processes"] = this.getInputData(1); //1
-    tmp["solver_settings"] = this.getInputData(2); //2
-    tmp["processes"]["initial_conditions_process_list"] = this.getInputData(3); //3
+    tmp["problem_data"] = this.getInputData(0);
+    tmp["output_processes"] = this.getInputData(1);
+    tmp["solver_settings"] = this.getInputData(2);
+
+    tmp["processes"] = {};
+    tmp["processes"]["initial_conditions_process_list"] = this.getInputData(3);
     tmp["processes"]["boundary_conditions_process_list"] = this.getInputData(4);
     tmp["processes"]["gravity"] = this.getInputData(5);
-    tmp["processes"]["auxiliar_process_list"] = this.getInputData(6); //6
+    tmp["processes"]["auxiliar_process_list"] = this.getInputData(6);
 
     this.setOutputData(0, tmp);
 };
 
-LiteGraph.registerNodeType("parameters/ProjectParameters", ProjectParameters);
+LiteGraph.registerNodeType("PARAMETERS/ProjectParameters", ProjectParameters);
 
 console.log("ProjectParameters node created"); //helps to debug
