083eec191ce81287089c59b684d9bdcfde131cdc
Marcelo Raschi <marcelo.raschi@gmail.com>
2022-10-03 15:13:49 +0200

    fix solver setting input type

--- js/nodes/project_parameters.js
+++ js/nodes/project_parameters.js
@@ -1,12 +1,12 @@
 function ProjectParameters() {
-    this.addInput("problem_data", "map");
+    this.addInput("problem_data", "map");  // #1
+    this.addInput("solver_settings", "solver_settings");  // #2
 //
-    this.addInput("solver_settings", "solver_settings");
-    this.addInput("initial_conditions_process_list", "process_array");
-    this.addInput("boundary_conditions_process_list", "process_array");
-    this.addInput("gravity", "map");
-    this.addInput("auxiliar_process_list", "process_array");
-    this.addInput("output_processes", "process_array");
+    this.addInput("initial_conditions_process_list", "process_array"); //2
+    this.addInput("boundary_conditions_process_list", "process_array");  //3
+    this.addInput("auxiliar_process_list", "process_array");  //4
+    this.addInput("gravity", "map");  //5
+    this.addInput("output_processes", "array");  //6
 
     this.addOutput("project_parameters","map");
     this.size = this.computeSize();
@@ -19,14 +19,14 @@ ProjectParameters.prototype.onExecute = function() {
     tmp = {};
 
     tmp["problem_data"] = this.getInputData(0);
-    tmp["output_processes"] = this.getInputData(1);
-    tmp["solver_settings"] = this.getInputData(2);
+    tmp["solver_settings"] = this.getInputData(1);
 
     tmp["processes"] = {};
-    tmp["processes"]["initial_conditions_process_list"] = this.getInputData(3);
-    tmp["processes"]["boundary_conditions_process_list"] = this.getInputData(4);
+    tmp["processes"]["initial_conditions_process_list"] = this.getInputData(2);
+    tmp["processes"]["boundary_conditions_process_list"] = this.getInputData(3);
+    tmp["processes"]["auxiliar_process_list"] = this.getInputData(4);
     tmp["processes"]["gravity"] = this.getInputData(5);
-    tmp["processes"]["auxiliar_process_list"] = this.getInputData(6);
+    tmp["output_processes"] = this.getInputData(6);
 
     this.setOutputData(0, tmp);
 };
