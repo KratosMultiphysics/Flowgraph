dc5b85c8e6603731d76365c1f70480645f40dd04
Marcelo Raschi <marcelo.raschi@gmail.com>
2022-10-25 14:32:08 +0200

    fix fluid solver I/O (wip)

--- js/nodes/SOLVERS/fluid_monolithic_solver.js
+++ js/nodes/SOLVERS/fluid_monolithic_solver.js
@@ -1,24 +1,54 @@
 class FluidMonolithicSolver {
-    constructor() {
-        // Identifier Glyph
-        this.glyph = {shape: '\uf085', font:'900 14px "Font Awesome 5 Free"', width: 16, height: 9};
 
-        // List of inputs and outputs ("name", "type")
-        this.addInput("Model", "modelpart_settings", {"glyph": {shape:"\uf6cf", font:'900 10px "Font Awesome 5 Free"', width:9, height:10}});  // 0
-        this.addInput("model_part_name", "string");  // 1
-        this.addInput("Volume submodelpart", "submodelpart");  // 2
-        this.addInput("Skin submodelparts", "submodelparts");  // 3
-        this.addInput("NonSkin submodelparts", "submodelparts");  // 4
-        this.addInput("Linear solver", "map");  // 5
-        this.addInput("Materials", "materials_settings");  // 6
-        this.addInput("Time stepping", "map");  // 7
-        
+    constructor() {
+
+        // node configuration
+
+        this.glyph = {
+            shape: '\uf085',
+            font: '900 14px "Font Awesome 5 Free"',
+            width: 16,
+            height: 9
+        };
+        this.size = this.computeSize();
+        //this.crop_title = 5;
+
+        // set inputs
+
+        let iidx = 0;
+        this.imod = iidx++;
+        this.addInput("Modelpart settings", "modelpart_settings", {
+            //"glyph": {
+            //    shape: "\uf6cf",
+            //    font: '900 10px "Font Awesome 5 Free"',
+            //    width: 9,
+            //    height: 10
+            //}
+        });
+        this.ivolmp = iidx++;
+        this.addInput("Volume submodelpart", "submodelpart");
+        this.iskinmp = iidx++;
+        this.addInput("Skin submodelparts", "submodelparts");
+        this.inskinmp = iidx++;
+        this.addInput("NonSkin submodelparts", "submodelparts");
+        this.ilinsol = iidx++;
+        this.addInput("Linear solver", "map");
+        this.imat = iidx++;
+        this.addInput("Materials", "materials_settings");
+        this.itim = iidx++;
+        this.addInput("Time stepping", "map");
+
+        // set outputs
+
+        let oidx = 0;
+        this.osolver = oidx++;
         this.addOutput("Solver", "solver_settings");
 
-        // List of properties
+        // properties
         this.properties = {
             "solver_type": "Monolithic",
             "model_import_settings": {},
+            "model_part_name": "",
             "echo_level": 0,
             "compute_reactions": false,
             "maximum_iterations": 10,
@@ -39,48 +69,60 @@ class FluidMonolithicSolver {
                 "dynamic_tau": 1.0
             },
             "reform_dofs_at_each_step": false,
-            "linear_solver_settings": {
-            }
+            "linear_solver_settings": {}
         };
-
-        this.domain_size = this.addWidget("combo","Domain Size", "2", function(v){}, { values:["2","3"]} );
-
-        this.size = this.computeSize();
-        this.crop_title = 5;
+        //this.domain_size = this.addWidget("combo", "Domain Size", "2", function(v) {}, { values: ["2", "3"] });
     }
 
     onExecute() {
         this._value = Object.assign({}, this.properties);
+	let val;
+	let idx;
 
-        //if (this.getInputData(0) != undefined) {
-        //    this._value["model_part_name"] = this.getInputData(1);
-        //}
-        this._value["model_import_settings"] = this.getInputData(0);
-        this._value["domain_size"] = this.domain_size.value;
-        this._value["volume_model_part_name"] = this.getInputData(2);
-        this._value["skin_parts"] = this.getInputData(3);
-        this._value["no_skin_parts"] = this.getInputData(4);
-        this._value["linear_solver_settings"] = this.getInputData(5);
+	idx = this.imod;
+        if (this.getInputData(idx) != undefined) {
+	    val = this.getInputData(idx)["mp_name"];
+            this._value["model_part_name"] = val;
+	}
 
-        if (this.getInputData(7) == undefined) {
-            // If the input is not provided, get the "problem_name" as mpda input file
-            if (this.getInputData(1) != undefined) {
-                this._value["model_import_settings"]["input_filename"] = this.getInputData(2)["problem_name"];
-            }
-            this._value["model_import_settings"]["input_type"] = "mdpa";
-        } else {
-            // Custom input provided
-            this._value["model_import_settings"] = this.getInputData(0);
-        }
+	idx = this.imod;
+        if (this.getInputData(idx) != undefined) {
+	    val = this.getInputData(idx)["mp_settings"];
+            this._value["model_import_settings"] =  val;
+	}
 
-        this._value["material_import_settings"] = this.getInputData(6);
+	idx = this.ivolmp;
+        if (this.getInputData(idx) != undefined) {
+        val = this.getInputData(idx)["smp_name"];
+        val = this.getInputData(idx)["mp_name"] + "." + this.getInputData(idx)["smp_name"];
+        this._value["volume_model_part_name"] = val;
+	}
 
-        // Get the 
-        this.setOutputData(0, this._value);
+	idx = this.iskinmp;
+        if (this.getInputData(idx) != undefined) {
+        //val = this.getInputData(this.iskinmp);
+        //this._value["skin_parts"] = val;
+	}
+
+	idx = this.inskinmp;
+        if (this.getInputData(idx) != undefined) {
+	    val = this.getInputData(idx);
+	    this._value["no_skin_parts"] = val;
+	}
+
+	idx = this.ilinsol;
+        if (this.getInputData(idx) != undefined) {
+            val = this.getInputData(idx);
+            this._value["linear_solver_settings"] = val;
+	}
+
+        //this._value["domain_size"] = this.domain_size.val;
+
+        this.setOutputData(this.osolver, this._value);
     }
 }
 
 FluidMonolithicSolver.title = "Fluid monolithic solver";
 FluidMonolithicSolver.desc = "Properties for the monolthic fluid solver";
 
-LiteGraph.registerNodeType("PROJECT PARAMETERS/Solvers/Fluid Monolithic Solver", FluidMonolithicSolver);
+LiteGraph.registerNodeType("SOLVERS/Fluid Monolithic Solver", FluidMonolithicSolver);
