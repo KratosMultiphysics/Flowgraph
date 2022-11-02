a32eca63768b977572ea816d3f12294cf57e2c1c
Marcelo Raschi <marcelo.raschi@gmail.com>
2022-11-02 13:16:29 +0100

    add formulation blocks

---
+++ js/nodes/SOLVERS/fluid_solver_formulations/formulation_fractional_step.js
@@ -0,0 +1,42 @@
+function FractionalStepFormulation() {
+
+        this.size = this.computeSize();
+
+        this.output = {
+            "type": "FrationalStep",
+            "formulation": {
+                "element_type": "",
+                "use_orthogonal_subscales": -1,
+                "dynamic_tau": -1
+            }
+        }
+
+        this.element_type = this.addWidget("combo", "Element", "vms",
+            function(v) {}, {
+                values: ["vms", "vms"]
+            });
+        this.use_subscale = this.addWidget("combo", "Orthogonal subsc", "false",
+            function(v) {}, {
+                values: ["false", "true"]
+            });
+        this.dynamic_tau = this.addWidget("number", "Dynamic tau ", 1.0,
+            function(v) {}, {
+                min: 0, max: 1,
+            });
+
+        this.addOutput("Formulation", "formulation");
+
+}
+
+MonolithicFormulation.prototype.onExecute = function() {
+
+        this.output["formulation"]["element_type"] = this.element_type.value;
+        this.output["formulation"]["use_orthogonal_subscales"] = this.use_subscale.value;
+        this.output["formulation"]["dynamic_tau"] = this.dynamic_tau.value;
+
+        this.setOutputData(0, this.output);
+}
+
+FractionalStepFormulation.title = "Fractional step formulation";
+FractionalStepFormulation.desc = "Description of the fractional step formulation";
+LiteGraph.registerNodeType("SOLVERS/Formulations/FractionalStep", FractionalStepFormulation);
