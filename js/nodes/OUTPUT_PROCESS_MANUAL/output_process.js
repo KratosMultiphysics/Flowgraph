67ab242f2f1e8ba32416e7afd60548799e129f67
Carlos Roig <roigcarlo@gmail.com>
2023-07-21 16:18:50 +0200

    fixing output_processes

---
+++ js/nodes/OUTPUT_PROCESS_MANUAL/output_process.js
@@ -0,0 +1,14 @@
+class OutputProcess {
+    constructor() {
+        // Identifier Glyph
+        this.glyph = {shape: '\uf303', font:'900 14px "Font Awesome 5 Free"', width: 16, height: 9};
+    }
+}
+
+// Set the colors of selected connection to better reflect the flow
+LGraphCanvas.link_type_colors["output_process"] = "#de9847";
+LGraphCanvas.slot_type_colors["output_process"] = "#de9847";
+LGraphCanvas.slot_type_colorsOff["output_process"] = "#de9847";
+LGraphCanvas.link_type_colors["output_process_list"] = "#de9847";
+LGraphCanvas.slot_type_colors["output_process_list"] = "#de9847";
+LGraphCanvas.slot_type_colorsOff["output_process_list"] = "#de9847";
