8620949155a343ed9ec3e421bb1644e82a42e27d
Carlos Roig <roigcarlo@gmail.com>
2023-09-18 16:22:55 +0200

    Added model_import_settings

---
+++ js/nodes/MODELPARTS/model_import_settings.js
@@ -0,0 +1,34 @@
+class ModelImportSettings {
+    constructor() {
+        super();
+        
+        this.addInput("name","string");
+        this.model_type = this.addWidget("combo", "Type", "mdpa", function(v) {}, { values: ["mdpa"] });
+
+        this.addOutput("Process","model_import_settings");
+
+        this.properties = {
+            "input_type": "mpda",
+            "input_name": ""
+        }
+
+        this.serialize_widgets = true;
+        this.size = this.computeSize();
+    }
+
+    onExecute = function() {
+        this._value = Object.assign({}, this.properties);
+
+        output["input_type"] = this.model_type.value;
+        output["input_name"] = this.getInputData(0)
+
+        this.setOutputData(0, this._value);
+    };
+}
+
+ModelImportSettings.title = "Model Import Settings";
+ModelImportSettings.desc = "Define inlet";
+
+LiteGraph.registerNodeType("Processes/ModelImportSettings", ModelImportSettings);
+
+console.log("ModelImportSettings node created"); //helps to debug
