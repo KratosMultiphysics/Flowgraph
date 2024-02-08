050f2b8d15caacf02f223cb5e578e56f535ff21b
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2024-02-08 17:15:30 +0100

    Remove modelers from stage postprocess

--- js/nodes/stages/post_process.js
+++ js/nodes/stages/post_process.js
@@ -8,7 +8,6 @@ class StagePostProcess {
         this.glyph = {shape: '\uf56e', font:'900 14px "Font Awesome 5 Free"', width: 16, height: 9};
 
         // List of inputs and outputs ("name", "type")
-        this.addInput("Modelers", "modelers_array");
         this.addInput("Operations", "operations_array");
 
         this.addOutput("Output", "stage_post");
