4b4d65436dc81da93a04f6b23182728c464e3b4d
Carlos Roig <roigcarlo@gmail.com>
2024-02-22 11:47:02 +0100

    adding missing processes for the potential flow stage

---
+++ public/js/nodes/processes/apply_far_field_process.js
@@ -0,0 +1,38 @@
+class ApplyFarFieldProcess extends Process {
+    constructor() {
+        super();
+        
+        this.addInput("model_part","string");
+        this.addOutput("Process","process_list");
+
+        this.angle_of_attack = this.addWidget("string","Angle of attack", "0.0", function(v){} );
+        this.mach_infinity = this.addWidget("string","Mach Infinity", "0.8", function(v){} );
+        this.speed_of_sound = this.addWidget("string","Sound Speed", "332.0", function(v){} );
+
+        this.serialize_widgets = true;
+        this.size = this.computeSize();
+    }
+
+    onExecute = function() {
+        let output = {
+            "python_module" : "apply_far_field_process",
+            "kratos_module" : "KratosMultiphysics.CompressiblePotentialFlowApplication",
+            "process_name"  : "FarFieldProcess",
+        }
+
+        output["Parameters"] = this.properties
+        output["Parameters"]["model_part_name"] = this.getInputData(0);
+        output["Parameters"]["angle_of_attack"] = parseFloat(this.angle_of_attack.value);
+        output["Parameters"]["mach_infinity"]   = parseFloat(this.mach_infinity.value);
+        output["Parameters"]["speed_of_sound"]  = parseFloat(this.speed_of_sound.value);
+
+        this.setOutputData(0, [output]);
+    };
+}
+
+ApplyFarFieldProcess.title = "Apply Far Field Process";
+ApplyFarFieldProcess.desc = "Applies a far field condition to a model part.";
+
+LiteGraph.registerNodeType("Processes/ApplyFarFieldProcess", ApplyFarFieldProcess);
+
+console.log("ApplyFarFieldProcess node created"); //helps to debug
---
+++ public/js/nodes/processes/define_wake_process_2d.js
@@ -0,0 +1,38 @@
+class DefineWakeProcess2d extends Process {
+    constructor() {
+        super();
+        
+        this.addInput("model_part","string");
+        this.addOutput("Process","process_list");
+
+        this.angle_of_attack = this.addWidget("string","Epsilon", "1e-9", function(v){} );
+        this.echo_level = this.addWidget("combo", "Echo level", 0, function(v) {}, {
+            values: [0, 1, 2, 3]
+        });
+
+        this.serialize_widgets = true;
+        this.size = this.computeSize();
+    }
+
+    onExecute = function() {
+        let output = {
+            "python_module" : "define_wake_process_2d",
+            "kratos_module" : "KratosMultiphysics.CompressiblePotentialFlowApplication",
+            "process_name"  : "DefineWakeProcess2D",
+        }
+
+        output["Parameters"] = this.properties
+        output["Parameters"]["model_part_name"] = this.getInputData(0)
+        output["Parameters"]["epsilon"]         = parseFloat(this.angle_of_attack.value);
+        output["Parameters"]["echo_level"]      = parseFloat(this.echo_level.value);
+
+        this.setOutputData(0, [output]);
+    };
+}
+
+DefineWakeProcess2d.title = "Define Wake Process 2D";
+DefineWakeProcess2d.desc = "Defines the Wake in 2D";
+
+LiteGraph.registerNodeType("Processes/DefineWakeProcess2d", DefineWakeProcess2d);
+
+console.log("DefineWakeProcess2d node created"); //helps to debug
